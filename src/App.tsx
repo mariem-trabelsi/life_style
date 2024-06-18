145740d6e8cc0017e2df787d478da6335cb8a73d
mariem-trabelsi <mariem.trabelsi@archicompass.com>
2024-06-18 17:48:00 +0100

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,24 @@
-// import React from 'react';
-// import logo from './logo.svg';
 import { useState } from 'react';
 import './App.css';
-import './Components/InputFieled'
 import InputFieled from './Components/InputFieled';
-let name: string
-let age:  number|string
-let hobbies: string []
-let printName: (name : string)=> void
+import { Todo } from './Components/todo.model';
 
-//  function printName1(n:string){
-//   console.log(n)
-//  }
-//  printName('meriem')
+const App:React.FC = () => {
+  const [todo, setTodo] = useState<string>('')
+  const [todos, setTodos] = useState<Todo[]>([])
+ console.log(todo)
+ const handleAddNewItem = (e:React.FormEvent)=> {
+  e.preventDefault()
+  if(todo) {
+    setTodos([...todos,{id: Date.now(), todo:todo, isDone:false}])
+    setTodo("")
+  }
+}
 
-// interface person{
-//   name: string
-//   age?:number
-// }
-
-function App() {
-  const [todo, setTodo]=useState<string>("")
   return (
     <div className="app">
       <span className="heading">taskify</span>
-      <InputFieled todo={todo} setTodo={setTodo}/>
+      <InputFieled todo={todo} setTodo={setTodo} handleAddNewItem={handleAddNewItem}/>
     </div>
   );
 }
--- src/Components/InputFieled.tsx
+++ src/Components/InputFieled.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
 interface Props{
-    todo:String;
+    todo:string;
     setTodo:React.Dispatch<React.SetStateAction<string>>;
-}
+    handleAddNewItem:(e:React.FormEvent)=> void
+};
 
-const InputFieled= ({todo, setTodo}:Props)=> {
+const InputFieled : React.FC<Props>= ({todo, setTodo, handleAddNewItem}) => {
  return(
     <form className="input">
         <input placeholder='Enter a task' className='input__box' type="input" value={todo} onChange={(e)=>setTodo(e.target.value)} />
-        <button className="input__submit" type="submit">Go</button>
+        <button className="input__submit" type="submit" onSubmit={handleAddNewItem}>Go</button>
     </form>
  )
     
